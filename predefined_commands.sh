75a27186b15d44332b8786fa99cd1b2a0502c776
Damian Pierzchalski <naod3sign@gmail.com>
2018-03-30 20:02:43 +0200

    added polish translation(only card name and only in prosperity addon and intrigue1stEdition)

---
+++ predefined_commands.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+dominion_dividers --papersize A4 --language pl --front blank --back none --no-page-footer --sleeved-thin --tab-side 'full' --orientation 'vertical' --base-cards-with-expansion --expansion 'prosperity' 'intrigue1stEdition' --edition '1' 
