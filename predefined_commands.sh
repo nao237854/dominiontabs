5a726b8ed32f193a7f1dd8839c56eb3249e224c6
Damian Pierzchalski <nao237854@gmail.com>
2023-05-21 15:16:06 +0200

    feat: update pl translations

--- predefined_commands.sh
+++ predefined_commands.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-dominion_dividers --papersize A4 --language pl --front blank --back none --no-page-footer --tab-side 'left-alternate' --orientation 'horizontal' --expansion 'dominion1stEdition' --edition '1' --tabwidth 6 --tab-name-align 'centre' --size '10.1x6.8' --no-tab-artwork --use-set-icon
+dominion_dividers --papersize A4 --language pl --front blank --back none --no-page-footer --tab-side 'left-alternate' --orientation 'horizontal' --expansion 'promo' --edition '1' --tabwidth 6 --tab-name-align 'centre' --size '9.4x6.3 --no-tab-artwork --use-set-icon --group-special --only-type-any 'action' 'artifact'
